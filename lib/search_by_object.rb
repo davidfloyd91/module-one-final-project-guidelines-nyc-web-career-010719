5d1f5f4d9e0df550df7ba338464173786c230771
purplesands <mullenjoe13@gmail.com>
2019-01-24 00:16:31 -0500

    tidied up from last commit

--- lib/search_by_object.rb
+++ lib/search_by_object.rb
@@ -1,6 +1,5 @@
-####################### obect obtainment
+############ SEARCHING THROUGH ARTWORKS ######
 
-#
 puts "Enter a keyword to search for: "
 input = gets.chomp
 artwork_array = []
@@ -17,18 +16,19 @@ artwork_api_result = RestClient::Request.execute(method: :get,
 artwork_array = JSON.parse(artwork_api_result)["records"]
 
 
-def print_search_results(arr)
+##### METHODS ######
+
+def print_artwork_results(arr)
   arr.each.with_index(0) do |data, index|
     puts "#{index}. " + data['title'][0..100]
     puts "\n"
   end
 end
 
+print_artwork_results(artwork_array)
 
-print_search_results(artwork_array)
-
-puts "Choose from 0-9 to get more information "
- input2 = gets.chomp.to_i
+puts "Choose from 0-9 to get more information " ###CLI
+ input2 = gets.chomp.to_i                       ###CLI
 
 def get_image_artist_name(arr, i)
   puts "Artist: " + arr[i]["people"][0]["name"]
@@ -42,8 +42,6 @@ def get_image_title(arr, i)
  puts "Title: " + arr[i]["title"]
 end
 
-
-
 # binding.pry
 puts get_image_title(artwork_array, input2)
 puts get_image_artist_name(artwork_array, input2)
@@ -51,18 +49,14 @@ puts get_image_url(artwork_array, input2)
 
 puts "Do you want to open the image?"
 
-def testing_testing(arr, i)
+def open_image(arr, i)
   imgurl = arr[i]["primaryimageurl"]
   system("open -a Safari #{imgurl}")
 end
 
-testing_testing(artwork_array, input2)
+open_image(artwork_array, input2)
 
-
-
-################SAME THING WITH ARTIST
-
-
+###########SEARCH BY ARTIST#############
 
 puts "Search by artist name"
 art_input = gets.chomp  #### this will be user input
@@ -78,17 +72,15 @@ artist_api_result = RestClient::Request.execute(method: :get,
       )
 artist_array = JSON.parse(artist_api_result)["records"]
 
-def print_search_results(arr)
+def print_artist_results(arr)
   arr.each.with_index(0) do |data, index|
     puts "#{index}. " + data['displayname']
     puts "\n"
   end
 end
 
-### INT WILL BE USER NUMBER CHOICE FROM LIST
-
 print_search_results(artist_array)
-#
+
 puts "Choose from 0-9 to get more information "
  art_input2 = gets.chomp.to_i
 
